5b9e93ff40640a0b5ecc2e8b48e3b983f23e33c2
Anna B <annab.@wirelessprv-10-195-202-53.near.illinois.edu>
2017-04-04 00:00:00 -0500

    added highlight feature

--- Navigate/Navigate.py
+++ Navigate/Navigate.py
@@ -3,33 +3,16 @@ import re
 
 
 class Navigate:
-    # def static navigate_file():
-    #     #  first number is the line and the second is the indentation
-    #     with open('sample.txt') as file:
-    #         for mark, line in enumerate(file.readlines()):
-    #             print(mark, len(re.findall("^ *", line)[0]))
-    #
-    #
-    # def static get_column():
-    #     with open("sample.txt") as file:
-    #         # splits a files in line
-    #         lines = file.readlines()
-    #
-    #     # extracts the stuff in the column
-    #     for line in lines[2:]:
-    #         columns = line.split()
-    #         print(columns[1])
-
     # away to get the line number of something we want
     @staticmethod
     def get_line(word='Why', filename="sample.txt"):
-
+        
         with open(filename) as file:
             for num, line in enumerate(file, 1):
                 if word in line:
                     return num
 
-    @staticmethod
+@staticmethod
     def get_column_number(filename="sample.txt"):
         with open(filename) as file:
             reader = csv.reader(file, delimiter=' ', skipinitialspace=True)
@@ -37,22 +20,20 @@ class Navigate:
             num_cols = len(first_row)
             print(num_cols)
 
-    @staticmethod
+@staticmethod
     def get_specific_column_number(word="do", filename="sample.txt"):
         file = open(filename,"r")
         list = []
         for line in file:
             fields = line.split(" ")
             list.append(fields)
-
-        # print(list)
+        
         for i in range(0, len(list)):
-            j = 1
+            j = 0
             for item in list[i]:
                 if word in item:
                     return j
-                j += 1
-
+                j += len(item) + 1
 
 
 
